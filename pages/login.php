be8cfcd44037c8725583838d1c1323ca93506372
Sm4rtSt1ck <tranzistor404@gmail.com>
2024-11-01 17:21:14 +0300

    Attempts to change form align

--- pages/login.php
+++ pages/login.php
@@ -1,10 +1,16 @@
 <h2>Вход</h2>
 <?php if (!empty($error)) echo "<p class='error'>$error</p>"; ?>
-<form action="pages/process_login.php" method="POST">
+<form action="pages/process_login.php" method="POST" class="reg_login_form>
     <label for="username">Имя пользователя:</label>
+    <br>
     <input type="text" name="username" required>
+    <br>
+    <br>
     <label for="password">Пароль:</label>
+    <br>
     <input type="password" name="password" required>
+    <br>
+    <br>
     <button type="submit">Войти</button>
 </form>
 <p>Нет аккаунта? <a href="index.php?page=register">Зарегистрироваться</a></p>
--- pages/register.php
+++ pages/register.php
@@ -1,12 +1,21 @@
 <h2>Регистрация</h2>
 <?php if (!empty($error)) echo "<p class='error'>$error</p>"; ?>
-<form action="pages/process_register.php" method="POST">
+<form action="pages/process_register.php" method="POST" class="reg_login_form">
     <label for="username">Имя пользователя:</label>
+    <br>
     <input type="text" name="username" required>
+    <br>
+    <br>
     <label for="email">Электронная почта:</label>
+    <br>
     <input type="email" name="email" required>
+    <br>
+    <br>
     <label for="password">Пароль:</label>
+    <br>
     <input type="password" name="password" required>
+    <br>
+    <br>
     <button type="submit">Зарегистрироваться</button>
 </form>
 <p>Уже есть аккаунт? <a href="index.php?page=login">Войти</a></p>
